877c572f41f5cc6b12cc24d4512d8b979439b1df
Deva Rugved <120798781+devarugved07012004@users.noreply.github.com>
2025-06-23 15:47:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,164 @@
+terraform{
+  backend "s3" {
+    bucket = "rugved-bucket-backend"
+    key = "backend"
+    region = "eu-north-1"
+  }
+}
 provider "aws" {
-  region = "eu-north-1" 
+  region = "eu-north-1" # You can change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "bucket-rugved-static" 
+# Define the S3 bucket
+resource "aws_s3_bucket" "my_terraform_bucket" {
+  bucket = "rugved-bucket" 
 
   website {
     index_document = "index.html"
+    error_document = "error.html"
   }
 
   tags = {
-    Project     = "StaticWebsiteDeployment"
-    Environment = "Production"
+    Environment = "Development"
+    Project     = "CodeBuildTerraformWebsite"
+    ManagedBy   = "Terraform"
   }
 }
 
-# Block Public Access resource 
-resource "aws_s3_bucket_public_access_block" "static_website_bucket_public_access_block" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  # False to allow public policies for static website hosting
-  block_public_acls       = false 
-  block_public_policy     = false
-  ignore_public_acls      = false 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_terraform_bucket.id
+  block_public_acls  = false
+  block_public_policy = false
+  ignore_public_acls = false
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.my_terraform_bucket.id
 
-
-resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  # Policy allows GetObject for public read
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": [
-          "${aws_s3_bucket.static_website_bucket.arn}/*"
-        ]
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:*",
+        Resource  = "${aws_s3_bucket.my_terraform_bucket.arn}/*"
       }
     ]
   })
-
-  depends_on = [aws_s3_bucket_public_access_block.static_website_bucket_public_access_block]
 }
 
-output "website_endpoint" {
-  value = aws_s3_bucket.static_website_bucket.website_endpoint
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.my_terraform_bucket.id
+  key = "index.html"
+  source = "${path.module}/index.html" # Assumes index.html is in the same folder as your .tf files
+  content_type = "text/html"
+}
+
+# CodeBuild Role
+resource "aws_iam_role" "codebuild_role" {
+  name = "CodeBuildServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name   = "CodeBuildPolicy"
+  role   = aws_iam_role.codebuild_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:*",
+          "s3:*",
+          "secretsmanager:GetSecretValue",
+          "ssm:GetParameter"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# CodeDeploy Role
+resource "aws_iam_role" "codedeploy_role" {
+  name = "CodeDeployServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codedeploy_policy" {
+  name   = "CodeDeployPolicy"
+  role   = aws_iam_role.codedeploy_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "autoscaling:*",
+          "ec2:DescribeInstances",
+          "cloudwatch:PutMetricData"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# CodePipeline Role
+resource "aws_iam_role" "codepipeline_role" {
+  name = "CodePipelineServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "CodePipelinePolicy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "codebuild:StartBuild",
+          "codedeploy:CreateDeployment",
+          "s3:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
